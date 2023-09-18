12a9a2fd48622478ae511255b1281882da8b225c
Tran Duoc <duoctran2527@gmail.com>
2023-09-18 17:50:23 +0700

    complete define route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,13 @@ import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
 import { ConnectDataBase } from './configs/mongoose.config'
+import routes from './routers'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('api/v1', routes)
 
 app.use('/', (req, res) => {
   return res.send('Hello')
